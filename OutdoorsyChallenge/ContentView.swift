fcbc8950cd5747365e2830901c6bc08503d516ed
Gabriela Bakalova <gaby.zagarova@gmail.com>
2023-10-02 13:56:43 +0300

    Initial Commit

---
+++ OutdoorsyChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OutdoorsyChallenge
+//
+//  Created by Gabriela Bakalova on 2.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OutdoorsyChallenge/OutdoorsyChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutdoorsyChallengeApp.swift
+//  OutdoorsyChallenge
+//
+//  Created by Gabriela Bakalova on 2.10.23.
+//
+
+import SwiftUI
+
+@main
+struct OutdoorsyChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
